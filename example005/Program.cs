d3824ca3cf330d895b458ee1cf77370b0548cfb2
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-14 19:35:47 +0300

    example005 work

--- example005/Program.cs
+++ example005/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if(username.ToLower() == "пипетка");
+if(username.ToLower() == "пипетка")
     Console.WriteLine("Ура, это же Пипетка!");
 else
     Console.Write("Привет, ");
