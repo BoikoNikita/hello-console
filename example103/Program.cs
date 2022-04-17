463a7f5c3b33872f26cbc1b4eb4881b7f2782f86
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-17 10:14:08 +0300

    example103

---
+++ example103/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 51,82,73,55,64,55,46,37,82,91 };
+
+int n = array.Length;
+int find = 55;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
