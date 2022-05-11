f414a31ae6ffac58caf7a7284bd363efc9b461ea
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-05-11 15:48:18 +0300

    example204

---
+++ example204/Program.cs
@@ -0,0 +1,10 @@
+﻿// f(n) = f(n-1) + f(n-2)
+double Fib (int n)
+{
+    if(n == 1 || n ==2) return 1;
+    else return Fib(n-1) + Fib(n-2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"Fib({i}) = {Fib(i)}");
+}
