3bbedf456bf7aacf858a053886ca611637856031
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-05-11 15:19:33 +0300

    example203

---
+++ example203/Program.cs
@@ -0,0 +1,18 @@
+﻿//int Factorial (int n)
+//{
+    //1! = 1
+    //0! = 1
+//    if (n==1) return 1;
+//    else return n * Factorial(n-1);
+//}
+//Console.WriteLine(Factorial(3));
+
+double Factorial (int n)
+{
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+    for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
